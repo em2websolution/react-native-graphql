8cd73f97579b60619f871824c8824b2b3f33c8f2
Eduardo Marques <psyfreak2012@gmail.com>
2023-01-18 15:49:57 -0300

    Graphql

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+import AsyncStorage from '@react-native-community/async-storage'
+import { persistCache } from 'apollo3-cache-persist'
+
+import Loading from "./src/Loading";
+import HomeScreen from "./src/HomeScreen";
+import ChapterScreen from './src/ChapterScreen'
+
+import { screenOptions } from "./src/styles";
+
+const Stack = createStackNavigator();
+
+const cache = new InMemoryCache()
+
+const client = new ApolloClient({
+  uri: "https://api.graphql.guide/graphql",
+  cache,
+  defaultOptions: { watchQuery: { fetchPolicy: 'cache-and-network' } },
+});
 
 export default function App() {
+  const [loadingCache, setLoadingCache] = useState(true)
+
+  useEffect(() => {
+    persistCache({
+      cache,
+      storage: AsyncStorage,
+    }).then(() => setLoadingCache(false))
+  }, [])
+
+  if (loadingCache) {
+    return <Loading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: "📖 The GraphQL Guide" }}
+          />
+          <Stack.Screen
+            name="Chapter"
+            component={ChapterScreen}
+            options={({
+              route: {
+                params: {
+                  chapter: { number, title },
+                },
+              },
+            }) => ({
+              title: number ? `Chapter ${number}: ${title}` : title,
+            })}
+          />
+        </Stack.Navigator>
+        <StatusBar style="light" />
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/HomeScreen.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Text, FlatList, Pressable } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+
+import Loading from "./Loading";
+import styles from "./styles";
+
+const CHAPTERS_QUERY = gql`
+  query Chapters {
+    chapters {
+      id
+      number
+      title
+    }
+  }
+`;
+
+const ChapterItem = ({ chapter, onPress }) => {
+  const { number, title } = chapter;
+  let header, subheader;
+
+  if (number) {
+    header = `Chapter ${number}`;
+    subheader = title;
+  } else {
+    header = title;
+  }
+
+  return (
+    <Pressable style={styles.item} onPress={onPress}>
+      <Text style={styles.header}>{header}</Text>
+      {subheader && <Text style={styles.subheader}>{subheader}</Text>}
+    </Pressable>
+  );
+};
+
+export default ({ navigation }) => {
+  const { data, loading } = useQuery(CHAPTERS_QUERY);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <FlatList
+      data={data.chapters}
+      renderItem={({ item }) => (
+        <ChapterItem
+          chapter={item}
+          onPress={() => navigation.navigate("Chapter", { chapter: item })}
+        />
+      )}
+      keyExtractor={(chapter) => chapter.id.toString()}
+    />
+  );
+};
